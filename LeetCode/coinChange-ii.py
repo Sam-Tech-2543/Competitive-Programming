552f884b6164a7abce48a892d8280c0162bf0721
SarveshMankar <mankarsarvesh2543@gmail.com>
2023-08-12 00:56:45 +0530

    Coin Change: Caching

--- LeetCode/coinChange-ii.py
+++ LeetCode/coinChange-ii.py
@@ -3,13 +3,14 @@
 
 from typing import List
 
+
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        cache=[0 for i in range(0,amount+1)]
-        cache[0]=1
+        cache = [0 for i in range(0, amount + 1)]
+        cache[0] = 1
 
         for i in coins:
-            for j in range(i,amount+1):
-                cache[j]+=cache[j-i]
+            for j in range(i, amount + 1):
+                cache[j] += cache[j - i]
 
         return cache[-1]
